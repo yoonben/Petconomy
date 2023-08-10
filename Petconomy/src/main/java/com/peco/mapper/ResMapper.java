b91472995edd6bb1d69423c4a35a05b2be941b95
user <user@DESKTOP-2PGRMF4>
2023-08-10 14:12:34 +0900

    병원

--- Petconomy/src/main/java/com/peco/mapper/ResMapper.java
+++ Petconomy/src/main/java/com/peco/mapper/ResMapper.java
@@ -2,6 +2,8 @@ package com.peco.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.peco.vo.H_RESVO;
 import com.peco.vo.HospitalVO;
 import com.peco.vo.MemberVO;
@@ -10,23 +12,23 @@ import com.peco.vo.PensionVO;
 
 public interface ResMapper {
 	
-	public List<MemberVO> getMemberList(); //멤버정보
+	public List<MemberVO> getMemberList(String m_id); //멤버정보
 	
-	public List<PensionVO> getPensionList();//펜션정보
+	public List<PensionVO> getPensionList(@Param(value="p_id") String p_id, @Param(value="room_no") String room_no);//펜션정보
 	
-	public List<P_RESVO> getPensionDisableDate();//펜션예약불가날짜
+	public List<P_RESVO> getPensionDisableDate(@Param(value="p_id") String p_id, @Param(value="room_no") String room_no);//펜션예약불가날짜
 	
-	public List<P_RESVO> getResPensionList(); //펜션예약내역조회
+	public List<P_RESVO> getResPensionList(String m_id); //펜션예약내역조회
 	
 	public int insertResvationPension(P_RESVO p_resVO); //펜션예약하기
 	
 	public int deleteResPension(String imp_uid); //펜션예약정보 삭제
 	
-	public List<HospitalVO> getHospitalList();//병원정보
+	public List<HospitalVO> getHospitalList(String h_id);//병원정보
 	
-	public List<H_RESVO> getHospitalDisableDate();//병원예약불가날짜
+	public List<H_RESVO> getHospitalDisableDate(String h_id);//병원예약불가날짜
 	
-	public List<H_RESVO> getResHospitalList(); //병원예약내역조회
+	public List<H_RESVO> getResHospitalList(String m_id); //병원예약내역조회
 	
 	public int insertResvationHospital(H_RESVO h_resVO); //병원예약하기
 	
--- Petconomy/src/main/java/com/peco/vo/H_RESVO.java
+++ Petconomy/src/main/java/com/peco/vo/H_RESVO.java
@@ -9,7 +9,7 @@ public class H_RESVO {
 	private String hr_date;
 	private String hr_pay;
 	private String h_id;
-	private String pname;
+	private String hname;
 	private String hr_name;
 	private String hr_email;
 	private String hr_tel;
